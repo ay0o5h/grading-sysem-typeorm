a105d131a1ddadf6ffc560b9270a699db7970f41
ay0o5h <aayosh553@gmail.com>
2021-09-27 18:37:08 +0300

    user controller

---
+++ routes/web/v1.ts
@@ -0,0 +1,22 @@
+import * as express from "express";
+import UserController from '../../controllers/web/user.controller';
+import otp from "../../middlewares/web/otp";
+import auth from "../../middlewares/web/auth";
+
+const route = express.Router();
+
+/// Not Auth
+route.post("/register", UserController.register);
+route.post("/otp", otp, UserController.checkOtp);
+route.post("/login", UserController.login);
+
+route.post("/forget/password", UserController.forget);
+route.post("/verify/password", UserController.verifyPassword);
+
+
+//  Need Auth
+route.use(auth);
+
+route.get("/check", UserController.check);
+
+export default route;
