f2035f03878c4e81a6c53491297dc33695c2a9ab
ay0o5h <aayosh553@gmail.com>
2021-12-10 16:54:46 +0300

    make it better

--- src/entity/Course.ts
+++ src/entity/Course.ts
@@ -1,6 +1,5 @@
 import { BaseEntity, Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Admin } from "./Admin";
-import { CourseEnrollment } from "./CourseEnrollment";
 import { Lectures } from "./Lectures";
 import { Test } from "./Test";
 import { User } from "./User";
@@ -28,8 +27,7 @@ export class Course extends BaseEntity {
   users: User[];
   @ManyToOne((type) => Admin, (admin) => admin.courses)
   admin: Admin;
-  @OneToMany((type) => CourseEnrollment, (courseEnrollment) => courseEnrollment.courses)
-  courseEnrollment: CourseEnrollment;
+
   @OneToMany((type) => Test, (test) => test.course)
   tests: Test[];
   @OneToMany((type) => Lectures, (lecture) => lecture.course)
